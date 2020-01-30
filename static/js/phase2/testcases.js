bb50b6f747089bce86837ed862baba6b1eac5103
Ye Tian <wafield.pku@gmail.com>
2020-01-29 20:50:49 -0800

    1

--- static/js/phase2/testcases.js
+++ static/js/phase2/testcases.js
@@ -81,38 +81,38 @@ define([
     }
   ];
   config.ground_truth = [
-    { // TC2
-      'input': 'police services are cut',
-      'nuggets': [86, 150, 170],
-      'slot_id': 3,
-      'extra_hits': [],
-    },{ // TC1
+    { // TC1
       'input': 'borough revenue remains static',
       'nuggets': [34, 36, 38, 70, 72, 76, 78, 82, 84, 
                   94, 122, 124, 126, 130, 154, 182, 
                   216, 226],
       'slot_id': 3,
       'extra_hits': ['property tax', 'lst', 'local service tax', 'expenditure', 'eit', 'earned income tax', 'visitor', 'transfer tax'],
-    },{ // TC3
+    }, { // TC2
+      'input': 'police services are cut',
+      'nuggets': [86, 150, 170],
+      'slot_id': 3,
+      'extra_hits': [],
+    }, { // TC3
       'input': 'property tax remains static',
       'nuggets': [94, 96, 102, 104, 106, 114, 116, 
                   118, 132, 198],
       'slot_id': 3,
       'extra_hits': ['assessment', 'reassessment', 'sell', 'landlock', 'landlocked', 'development', 'expansion'],
-    },{ // TC5
-      'input': 'fixed income',
-      'nuggets': [80, 110, 166],
-      'slot_id': 7,
     },{ // TC4
       'input': 'employee salaries are increasing',
       'nuggets': [140, 202, 204, 206],
       'slot_id': 3,
       'extra_hits': ['benefits', 'health care', 'retirement benefits', 'pension plan'],
-    },{ //TC6
+    }, { //TC5
       'input': 'appealing to homeowner',
       'nuggets': [90, 162, 208, 220],
       'slot_id': 6,
       'extra_hits': [],
+    }, { // TC6
+      'input': 'fixed income',
+      'nuggets': [80, 110, 166],
+      'slot_id': 7,
     }
   ];
 
